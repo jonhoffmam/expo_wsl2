ee0446981a91d57fb6bbcd11f571e0da3ef75d63
Jonathan Hoffmam Pivetta <jon.hoffmam@yahoo.com.br>
2020-09-10 23:38:59 -0300

    Refactored code

--- wsl_hosts.ps1
+++ wsl_hosts.ps1
@@ -1,10 +1,28 @@
-$currentPathWin = (Get-Location).Path;
-$diskDrive = $currentPathWin.Substring(0,1);
-$currentPathWsl = $currentPathWin.Replace("$($diskDrive):\","/mnt/$($diskDrive.ToLower())/").Replace("\","/");
 $getDate = (Get-Date -Format d).replace("/","-");
 $getTime = (Get-Date -Format T);
-$outPutFirewall = @();
-$outFileLog = @();
+$timeStamp = (Get-Date -Format d).replace("/","_");
+
+$configFolderPath = "C:\Users\$env:USERNAME\AppData\Local\wsl2_host_config";
+$configFilePath = "$configFolderPath\$timeStamp.config.log";
+
+if (Test-Path "$configFolderPath\*.log") {
+	$currentPathWin = (Get-Content "$configFolderPath\*.log")[-3]
+} else {
+	$currentPathWin = (Get-Location).Path;
+}
+
+$diskDrive = $configFolderPath.Substring(0,1);
+$configFolderPathWsl = $configFolderPath.Replace("$($diskDrive):\","/mnt/$($diskDrive.ToLower())/").Replace("\","/");
+$outPutConfigPath = [PSCustomObject]@{
+	SCRIPT_PATH = $currentPathWin;
+};
+
+if (!(Test-Path $configFolderPath)) {
+	Write-Output "Creating config folder...`n"
+	mkdir $configFolderPath | Out-Null;
+}
+
+Remove-Item "$configFolderPath\*.config.log" -EV Err -EA SilentlyContinue;
 
 #[GET THE INTERFACE NAME]
 $interfaceName = @();
@@ -23,33 +41,37 @@ foreach ($name in $interface) {
 }
 
 #[REG KEY ON WINDOWS]
-$foundReg = Invoke-Expression "REG QUERY 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\wsl2host.exe' | findstr 'wsl2host.exe'";
+$registryPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\wsl2host.exe';
 
-if (!$foundReg) {
-	Write-Output "'wsl2host' key registering on Windows...`n";
-	Invoke-Expression "REG ADD 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\wsl2host.exe' /d '$currentPathWin\start.bat'" | Out-Null;
-	Invoke-Expression "REG ADD 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\wsl2host.exe' /v 'Path' /d '$currentPathWin'" | Out-Null;
+if (!(Test-Path $registryPath)) {
+	Write-Output "'wsl2host' key registering on Windows...`n";	
+	New-Item -Path $registryPath -Value "$currentPathWin\start.bat" -Force | Out-Null;
+	New-ItemProperty -Path $registryPath -Name 'Path' -Value $currentPathWin -PropertyType string -Force | Out-Null;	
 } else {
 	Write-Output "'wsl2host' key already exist...`n";
 }
 
 #[SHEDULE TASK ON WINDOWS]
-$foundTask = Invoke-Expression "SCHTASKS /QUERY /TN 'WSL2HOST' | findstr 'WSL'";
+$foundTask = Get-ScheduledTask -TaskName 'WSL2HOST' -EA SilentlyContinue;
 
 if (!$foundTask) {
 	Write-Output "Schedule task creating...`n";
-	Invoke-Expression "SCHTASKS /CREATE /TN 'WSL2HOST' /SC ONLOGON /TR 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass $currentPathWin\wsl_hosts.ps1'" | Out-Null;
+	$taskAction = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass $currentPathWin\wsl_hosts.ps1";
+	$taskTrigger = New-ScheduledTaskTrigger -AtLogon;
+	$taskPrincipal = New-ScheduledTaskPrincipal -UserId "$env:USERDOMAIN\$env:USERNAME" -RunLevel Highest;	
+	$task = New-ScheduledTask -Action $taskAction -Principal $taskPrincipal -Trigger $taskTrigger;
+	Register-ScheduledTask -TaskName "WSL2HOST" -InputObject $task | Out-Null;	
 } else {
 	Write-Output "Schedule task already exist...";
 }
 
 #[INSERT env REACT_NATIVE_PACKAGER_HOSTNAME IN .bashrc AND .zshrc]
 $foundEnv = @(
-	[pscustomobject]@{
+	[PSCustomObject]@{
 		value = bash.exe -c "grep 'REACT_NATIVE_PACKAGER_HOSTNAME' ~/.bashrc";
 		path = "~/.bashrc";
 	};	
-	[pscustomobject]@{
+	[PSCustomObject]@{
 		value = bash.exe -c "grep 'REACT_NATIVE_PACKAGER_HOSTNAME' ~/.zshrc";
 		path = "~/.zshrc";
 	};
@@ -62,10 +84,10 @@ foreach ( $item in $foundEnv ) {
 			$userWsl = bash.exe -c "whoami";
 			bash.exe -c "sudo chown $userWsl`:$userWsl $path"
 		}
-		$keepAddress = "`n# Set env for EXPO `nlocalAddress=``awk 'NR==4 {print `$3}' $currentPathWsl/*.log | sed 's/\\r//g'`` `nexport REACT_NATIVE_PACKAGER_HOSTNAME=`$localAddress` `n# End";
-		$keepAddress | out-file -encoding "ASCII" "$currentPathWin\temp.txt";
-		bash.exe -c "cat $currentPathWsl/temp.txt >> $path ";
-		Remove-Item "$currentPathWin\temp.txt" -EV Err -EA SilentlyContinue;
+		$keepAddress = "`n# Set env for EXPO `nlocalAddress=``awk 'NR==4 {print `$3}' $configFolderPathWsl/*.config.log | sed 's/\\r//g'`` `nexport REACT_NATIVE_PACKAGER_HOSTNAME=`$localAddress` `n# End";
+		$keepAddress | out-file -encoding "ASCII" "$configFolderPath\temp.txt";
+		bash.exe -c "cat $configFolderPathWsl/temp.txt >> $path ";
+		Remove-Item "$configFolderPath\temp.txt" -EV Err -EA SilentlyContinue;
 	}
 }
 
@@ -81,7 +103,13 @@ foreach ( $tempAddress in $interfaceName ) {
 
 if ($foundLocalAddress) {
 	$localAddress = $matches[0];	
-	$outPutInterface = @([pscustomobject]@{INTERFACE=$tempAddress; LOCAL_ADDRESS=$localAddress; STATUS="OK"; DATE = $getDate; TIME = $getTime});
+	$outPutInterface = @([PSCustomObject]@{
+		INTERFACE = $tempAddress;
+		LOCAL_ADDRESS = $localAddress;
+		STATUS = "OK";
+		DATE = $getDate;
+		TIME = $getTime
+	});
 
 	} else {
 	Write-Output "The Script Exited, the IP address of Windows cannot be found";
@@ -101,11 +129,11 @@ if ($foundRemoteAddress) {
 
 #[FIREWALL]
 $firewall = @(
-	[pscustomobject]@{
+	[PSCustomObject]@{
 		type="TCP"; 
 		name="WSL2 Firewall Unlock TCP"; 
 		ports=@(19000,19001,19002,19003,19004,19005)}
-	[pscustomobject]@{
+	[PSCustomObject]@{
 		type="UDP"; 
 		name="WSL2 Firewall Unlock UDP"; 
 		ports=@(53)}
@@ -122,34 +150,53 @@ foreach ( $item in $firewall) {
 	if ((!(Get-NetFireWallRule -DisplayName $nameFirewall -EV Err -EA SilentlyContinue)) -and ($typeFirewall -eq "TCP")) {      
 		Invoke-Expression "New-NetFireWallRule -DisplayName '$nameFirewall' -Direction Outbound -LocalPort $portsTCP -Action Allow -Protocol TCP";
 		Invoke-Expression "New-NetFireWallRule -DisplayName '$nameFirewall' -Direction Inbound -LocalPort $portsTCP -Action Allow -Protocol TCP";    
-		$outPutFirewall += @([pscustomobject]@{FIREWALL_NAME=$nameFirewall; STATUS="CREATED"; DATE = $getDate; TIME = $getTime});
+		$outPutFirewall += @([PSCustomObject]@{
+			FIREWALL_NAME = $nameFirewall;
+			STATUS = "CREATED";
+			DATE = $getDate;
+			TIME = $getTime
+		});
 
 	} elseif ((!(Get-NetFireWallRule -DisplayName $nameFirewall -EV Err -EA SilentlyContinue)) -and ($typeFirewall -eq "UDP")) {    
 		Invoke-Expression "New-NetFireWallRule -DisplayName '$nameFirewall' -Direction Outbound -LocalPort $portsUDP -Action Allow -Protocol UDP";
 		Invoke-Expression "New-NetFireWallRule -DisplayName '$nameFirewall' -Direction Inbound -LocalPort $portsUDP -Action Allow -Protocol UDP";
-		$outPutFirewall += @([pscustomobject]@{FIREWALL_NAME=$nameFirewall; STATUS="CREATED"; DATE = $getDate; TIME = $getTime});
+		$outPutFirewall += @([PSCustomObject]@{
+			FIREWALL_NAME = $nameFirewall;
+			STATUS = "CREATED";
+			DATE = $getDate;
+			TIME = $getTime
+		});
 		
 	} else {
-		$outPutFirewall += @([pscustomobject]@{FIREWALL_NAME=$nameFirewall; STATUS="EXIST"; DATE = $getDate; TIME = $getTime});		
+		$outPutFirewall += @([PSCustomObject]@{
+			FIREWALL_NAME = $nameFirewall;
+			STATUS = "EXIST";
+			DATE = $getDate;
+			TIME = $getTime
+		});		
 	}
 }
 
 #[REDIRECTING PORTS]
-foreach ( $port in $firewall[0].ports) {		
+foreach ( $port in $firewall[0].ports) {
 	Invoke-Expression "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$localAddress" | Out-Null;
 	Invoke-Expression "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$localAddress connectport=$port connectaddress=$remoteAddress" | Out-Null;	
-	$outFileLog += @([pscustomobject]@{PORT= $port; LOCAL_ADDRESS = $localAddress; REMOTE_ADDRESS = $remoteAddress; DATE = $getDate; TIME = $getTime});
+	$outPutPorts += @([PSCustomObject]@{
+		PORT = $port;
+		LOCAL_ADDRESS = $localAddress;
+		REMOTE_ADDRESS = $remoteAddress;
+		DATE = $getDate;
+		TIME = $getTime
+	});
 }
 
 #[LOG]
-Remove-Item "$currentPathWin\*.log" -EV Err -EA SilentlyContinue;
-$timeStamp = (Get-Date -Format d).replace("/","_");
-$pathLog = "$currentPathWin\$timeStamp.log";
-
-$outPutInterface | Format-Table -Property STATUS,INTERFACE,LOCAL_ADDRESS,DATE,TIME -AutoSize | out-file -encoding "ASCII" $pathLog -append;
-$outPutFirewall | Format-Table -Property STATUS,FIREWALL_NAME,DATE,TIME -AutoSize | out-file -encoding "ASCII" $pathLog -append;
-$outFileLog | Format-Table -Property PORT,LOCAL_ADDRESS,REMOTE_ADDRESS,DATE,TIME -AutoSize | out-file -encoding "ASCII" $pathLog -append;
+$outPutInterface | Format-Table -Property STATUS,INTERFACE,LOCAL_ADDRESS,DATE,TIME -AutoSize | Out-File -Encoding "ASCII" $configFilePath -Append;
+$outPutFirewall | Format-Table -Property STATUS,FIREWALL_NAME,DATE,TIME -AutoSize | Out-File -Encoding "ASCII" $configFilePath -Append;
+$outPutPorts | Format-Table -Property PORT,LOCAL_ADDRESS,REMOTE_ADDRESS,DATE,TIME -AutoSize | Out-File -Encoding "ASCII" $configFilePath -Append;
+$outPutConfigPath | Format-Table -Property SCRIPT_PATH -AutoSize | Out-File -Encoding "UTF8" $configFilePath -Append;
 
 Write-Output $outPutInterface | Format-Table -Property STATUS,INTERFACE,LOCAL_ADDRESS,DATE,TIME -AutoSize;
 Write-Output $outPutFirewall | Format-Table -Property STATUS,FIREWALL_NAME,DATE,TIME -AutoSize;
-Write-Output $outFileLog | Format-Table -Property PORT,LOCAL_ADDRESS,REMOTE_ADDRESS,DATE,TIME -AutoSize;
+Write-Output $outPutPorts | Format-Table -Property PORT,LOCAL_ADDRESS,REMOTE_ADDRESS,DATE,TIME -AutoSize;
+Write-Output $outPutConfigPath | Format-Table -Property SCRIPT_PATH -AutoSize;
